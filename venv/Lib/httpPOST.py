602f1667d307e0af951d913c7c8f6a76cc3cbbec
Unknown <markgpmcpe@gmail.com>
2021-12-18 02:42:25 -0500

    Fix httpPOST.py
    Cyberstart has updated their challenges and is now using python 3, figured I would update this for you.

--- venv/Lib/httpPOST.py
+++ venv/Lib/httpPOST.py
@@ -3,23 +3,17 @@
 # GET / returns basic info about api. POST / with x-api-key:tweetbotkeyv1
 # and data with user tweetbotuser and status-update of alientest
 #
-
-
-import urllib2
 import urllib
+from urllib.request import Request, urlopen  # Python 3
 
-request = urllib2.Request("http://127.0.0.1:8082")
-response = urllib2.urlopen(request)
-html = response.read()
-print(html)
 
-data = urllib.urlencode({'user' : 'tweetbotuser', 'status-update'  : 'alientest'})
-request = urllib2.Request("http://127.0.0.1:8082", headers={'x-api-key':'tweetbotkeyv1'}, data=data)
-response = urllib2.urlopen(request)
-print response.read()
+req = Request("http://127.0.0.1:8082")
+req.add_header('x-api-key', 'tweetbotkeyv1')
+data = urllib.parse.urlencode({'user' : 'tweetbotuser', 'status-update'  : 'alientest'})
+data = data.encode()
+content = urlopen(req, data).read()
+print(content)
 
-'''
-<html><body><h1>I am TweetBot!</h1><p>Post with header x-api-key</p><<p>Parameters should include user and status-update.</p>/body></html>
-{"success": "true", "message":"Well done.", "flag":"Well done the flag is: DXDAGMLVCWX "}
-
-'''
+```
+{"success": "true", "message":"Well done.", "flag":"Well done the flag is: 1nQORVQMbfuRA1a0AUgr"}'
+```
